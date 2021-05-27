3628de8dacb57d264e447fd404a694a2ddad6c9f
farrukh.sadykov <farrukh.sadykov@capgemini.com>
2021-05-26 20:21:48 -0500

    added someone elses module

--- vpc/provider.tf
+++ vpc/provider.tf
@@ -1,48 +1,21 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "us-west-1"
 }
-
 module "vpc" {
-	source = "farrukh90/vpc/aws"
-	region = "us-east-1"
-	vpc_cidr = "10.0.0.0/16"
-	public_cidr1 = "10.0.1.0/24"
-	public_cidr2 = "10.0.2.0/24"
-	public_cidr3 = "10.0.3.0/24"
-	private_cidr1 = "10.0.101.0/24"
-	private_cidr2 = "10.0.102.0/24"
-	private_cidr3 = "10.0.103.0/24"
-	tags = {
-	Name = "VPC_Project"
-	Environment = "Dev"
-	Team = "DevOps"
-	Created_by = "Farrukh"
-	Department = "IT"
-	Bill = "CFO"
-	}
-}
+  source = "terraform-aws-modules/vpc/aws"
 
-output "vpc" {
-	value = "${module.vpc.vpc}"
-}
-output "private_subnet1" {
-	value = "${module.vpc.private_subnets[0]}"
-}
-output "private_subnet2" {
-	value = "${module.vpc.private_subnets[1]}"
-}
-output "private_subnet3" {
-	value = "${module.vpc.private_subnets[2]}"
-}
-output "public_subnet1" {
-	value = "${module.vpc.public_subnets[0]}"
-}
-output "public_subnet2" {
-	value = "${module.vpc.public_subnets[1]}"
-}
-output "public_subnet3" {
-	value = "${module.vpc.public_subnets[2]}"
-}
-output "region" {
-	value = "${module.vpc.region}"
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
